b840fe6348aeccddd60c9470f34b46bb9119c096
wilix-lead <lead@w.wilix.ru>
2017-01-13 19:57:54 +0300

    Prepare auto compile and auto clean after building

--- install.js
+++ install.js
@@ -3,15 +3,28 @@
 const proc = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
+
 if (process.argv[2] == 'install') {
   try {
     require('./index.js');
   } catch (e) {
     // Need compile
     console.log('Recompiling iohook for your environment');
+
+    let currentPlatform = 'iohook_';
+    currentPlatform += os.platform() + '_' + os.arch();
+    console.log('Platform is:', currentPlatform);
+    
     proc.execSync('git submodule update --init');
     proc.execSync('npm install nan cmake-js');
     proc.execSync('npm run compile');
+    if (fs.existsSync(path.join(__dirname, 'build', 'Release', 'iohook.node'))) {
+      fs.renameSync(
+        path.join(__dirname, 'build', 'Release', 'iohook.node'),
+        path.join(__dirname, 'build', 'Release', currentPlatform + '.node')
+      );
+    }
   }
 } else {
   proc.execSync('npm remove nan cmake-js');
