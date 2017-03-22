649e5efee07251cf6eb813eca3fcacf79f139b24
Koen Schmeets <k.schmeets@gmail.com>
2017-03-22 01:03:32 +0100

    Add build script and appveyor config

---
+++ build.js
@@ -0,0 +1,100 @@
+var prebuild = require('prebuild/prebuild')
+var upload = require('prebuild/upload')
+var spawn = require('child_process').spawn
+var async = require('async')
+var extend = require('xtend')
+
+const run = (args, cb) => {
+  args = args.map(arg => {
+    if (arg.indexOf('--target') > -1) {
+      return arg.replace('--target', '--runtime-version')
+    }
+    return arg
+  })
+  const proc = spawn('./node_modules/.bin/cmake-js', args, { env: process.env })
+  proc.stdout.pipe(process.stdout)
+  proc.stderr.pipe(process.stderr)
+  proc.on('exit', (code, sig) => {
+    if (code === 1) {
+      return cb(new Error(`Failed with signal: ${sig}`))
+    }
+    cb()
+  })
+}
+
+const cmakeWrap = {
+  todo: [],
+  parseArgv: function (argv) {
+    this.todo.push({
+      name: argv[2],
+      args: argv.slice(2)
+    })
+  },
+  commands: {
+    build: run,
+    clean: run,
+    configure: run,
+    rebuild: run,
+    update: run,
+    install: run,
+    list: run
+  }
+}
+
+var opts = {
+  target: process.versions.node,
+  runtime: 'node',
+  arch: process.arch,
+  libc: process.env.LIBC,
+  platform: process.platform,
+  all: false,
+  force: false,
+  debug: false,
+  verbose: false,
+  path: '.',
+  log: {
+    verbose: console.log.bind(console),
+    error: console.log.bind(console)
+  },
+  pkg: require('./package.json'),
+  gyp: cmakeWrap,
+  prebuild: require('node-abi').supportedTargets,
+}
+console.log(opts.prebuild)
+
+var files = []
+async.eachSeries(opts.prebuild, function (target, next) {
+  prebuild(opts, target.target, target.runtime, function (err, tarGz) {
+    if (err) return next(err)
+    files.push(tarGz)
+    next()
+  })
+}, function (err) {
+  if (err) return onbuilderror(err)
+  uploadFiles(files)
+})
+
+function uploadFiles (files) {
+  console.log('Uploading ' + files.length + ' prebuilds(s) to Github releases')
+  upload(extend(opts, {files: files}), function (err, result) {
+    if (err) return onbuilderror(err)
+    console.log('Found ' + result.old.length + ' prebuild(s) on Github')
+    if (result.old.length) {
+      result.old.forEach(function (build) {
+        console.log('-> ' + build)
+      })
+    }
+    console.log('Uploaded ' + result.new.length + ' new prebuild(s) to Github')
+    if (result.new.length) {
+      result.new.forEach(function (build) {
+        console.log('-> ' + build)
+      })
+    }
+  })
+}
+
+function onbuilderror (err) {
+  if (!err) return
+  console.error(err)
+  process.exit(2)
+}
