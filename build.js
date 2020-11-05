98d291b4b08a5c2fe8a401e3b0c258143d1e23df
ted <ykhwong@daum.net>
2020-11-06 04:55:15 +0900

    Fix typo

--- build.js
+++ build.js
@@ -99,12 +99,14 @@ function build(runtime, version, abi) {
 		args.push('--dist-url=https://atom.io/download/electron');
     }
 
-        if (arch === "x64" && parseInt(abi) >= 80) {
-                args.push('--v8_enable_pointer_compression=1');
-        } else if (arch === "x86" && parseInt(abi) >= 80) {
+    if (parseInt(abi) >= 80) {
+	    if (arch === "x64") {
+		args.push('--v8_enable_pointer_compression=1');
+	    } else {
                 args.push('--v8_enable_pointer_compression=0');
                 args.push('--v8_enable_31bit_smis_on_64bit_arch=1');
-        }
+	    }
+    }
 
     console.log('Compiling iohook for ' + runtime + ' v' + version + '>>>>');
 	if (process.platform === 'win32') {
